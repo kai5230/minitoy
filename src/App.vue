905139bc26d210c9fc500a8a74b8e2c47cf007cb
kai5230 <130663328+kai5230@users.noreply.github.com>
2023-04-14 09:25:30 +0000

    init framework

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <NConfigProvider
+    v-if="!isLock"
+    :locale="zhCN"
+    :theme="getDarkTheme"
+    :theme-overrides="getThemeOverrides"
+    :date-locale="dateZhCN"
+  >
+    <AppProvider>
+      <RouterView />
+    </AppProvider>
+  </NConfigProvider>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script lang="ts" setup>
+  import { computed } from 'vue';
+  import { zhCN, dateZhCN, darkTheme } from 'naive-ui';
+  import { AppProvider } from '@/components/Application';
+  import { useDesignSettingStore } from '@/store/modules/designSetting';
+  import { lighten } from '@/utils/index';
+
+  const designStore = useDesignSettingStore();
+  const isLock = computed(() => false);
+  /**
+   * @type import('naive-ui').GlobalThemeOverrides
+   */
+  const getThemeOverrides = computed(() => {
+    const appTheme = designStore.appTheme;
+    const lightenStr = lighten(designStore.appTheme, 6);
+    return {
+      common: {
+        primaryColor: appTheme,
+        primaryColorHover: lightenStr,
+        primaryColorPressed: lightenStr,
+        primaryColorSuppl: appTheme,
+      },
+      LoadingBar: {
+        colorLoading: appTheme,
+      },
+    };
+  });
+  const getDarkTheme = computed(() => (designStore.darkTheme ? darkTheme : undefined));
+
+</script>
+
+<style lang="less">
+  @import 'styles/index.less';
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,48 @@
+import type { UserConfig, ConfigEnv } from 'vite';
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base:'./'
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ],
+  resolve: {
+    alias: [
+      {
+        find: /\/#\//,
+        replacement: pathResolve('types') + '/',
+      },
+      {
+        find: '@',
+        replacement: pathResolve('src') + '/',
+      },
+    ],
+    dedupe: ['vue'],
+  }
 })
